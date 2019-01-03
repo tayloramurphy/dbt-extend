ec7eb622eba2e0062312bb382ad14e986d9c9536
Claus Herther <claus@calogica.com>
2019-01-02 16:07:56 -0800

    Updating tz namespace

--- macros/date/get_local_time.sql
+++ macros/date/get_local_time.sql
@@ -1,4 +1,4 @@
 {%- macro get_local_time(tz=None) -%}
-{% set tz = var("dbt_extend.time_zone") if not tz else tz %}
+{% set tz = var("dbt_extend:time_zone") if not tz else tz %}
 {{ dbt_extend.to_local_tz('current_timestamp::timestamp_ntz', tz) }}
 {%- endmacro -%}
--- macros/date/n_weeks_ago.sql
+++ macros/date/n_weeks_ago.sql
@@ -1,4 +1,4 @@
 {%- macro n_weeks_ago(n, tz=None) -%}
-{% set tz = var("dbt_extend.time_zone") if not tz else tz %}
+{% set tz = var("dbt_extend:time_zone") if not tz else tz %}
 date_trunc('week', dateadd('week', -1*{{ n }}, {{ dbt_extend.get_local_date(tz) }} ))
 {%- endmacro -%}
