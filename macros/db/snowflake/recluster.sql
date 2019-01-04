216a5b836e5739dc8185544cf7b91b8a2c8a5d62
Claus Herther <claus@calogica.com>
2019-01-04 09:39:39 -0800

    Adds support for conditional reclustering

--- macros/db/snowflake/recluster.sql
+++ macros/db/snowflake/recluster.sql
@@ -1,4 +1,11 @@
-{% macro recluster(this, col) -%}
- alter table {{ this }} cluster by ({{ col }});
- alter table {{ this }} resume recluster
- {% endmacro %}
+{% macro recluster(this, col, target_name=None) -%}
+{% set do = true %}
+{% if target_name and target.name == target_name %}
+    {% set do = false %}
+{% endif %}
+
+{% if do %}
+alter table {{ this }} cluster by ({{ col }});
+alter table {{ this }} resume recluster;
+{% endif %}
+{% endmacro %}
