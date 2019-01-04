0e07a1c663ca6aba04ddbdd069210d112cd61996
Claus Herther <claus@calogica.com>
2019-01-04 09:48:14 -0800

    Fixed conditional

--- macros/db/snowflake/recluster.sql
+++ macros/db/snowflake/recluster.sql
@@ -1,7 +1,11 @@
 {% macro recluster(this, col, target_name=None) -%}
 {% set do = true %}
-{% if target_name and target.name == target_name %}
+{% if target_name %}
+    {% if target.name == target_name %}
+    {% set do = true %}
+    {% else %}
     {% set do = false %}
+    {% endif %}
 {% endif %}
 
 {% if do %}
