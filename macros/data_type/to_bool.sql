61b4da874a71fe04ceaf6d442ae3b7b3a773ff1b
Claus Herther <claus@calogica.com>
2019-01-08 17:54:49 -0800

    Updates white space handling

--- macros/data_type/to_bool.sql
+++ macros/data_type/to_bool.sql
@@ -1,1 +1,1 @@
-{% macro to_bool(val, true_val=1, false_val=0) %}case when coalesce({{ val }}, {{ false_val }}) = {{ true_val }} then true else false end{% endmacro %}
+{%- macro to_bool(val, true_val=1, false_val=0) -%}case when coalesce({{ val }}, {{ false_val }}) = {{ true_val }} then true else false end{%- endmacro -%}
--- macros/schema_tests/equal_expression.sql
+++ macros/schema_tests/equal_expression.sql
@@ -1,13 +1,12 @@
-{% macro get_columns(group_by) %}
-    {% if group_by %}
-        {% for g in group_by %}
-            {{ g }} as col_{{ loop.index }}
-            {%- if not loop.last %}, {% endif -%}
-        {% endfor %}
-    {% endif %}
-{% endmacro %}
+{%- macro get_columns(group_by) -%}
+    {%- if group_by -%}
+        {%- for g in group_by -%}
+            {{ g }} as col_{{ loop.index }}{% if not loop.last %}, {% endif %}
+        {%- endfor -%}
+    {%- endif -%}
+{%- endmacro -%}
 
-{% macro get_select(model, expression, filter_cond, group_by) %}
+{%- macro get_select(model, expression, filter_cond, group_by) -%}
     select
         {{ get_columns(group_by) }},
         {{ expression }} as expression
@@ -18,16 +17,16 @@
         {{ filter_cond }}
     {% endif %}
     {{ dbt_utils.group_by(group_by|length) }}
-{% endmacro %}
+{%- endmacro -%}
 
-{% macro test_equal_expression(model, expression, 
+{%- macro test_equal_expression(model, expression, 
                                 compare_model=None, 
                                 compare_expression=None, 
                                 group_by=["'col'"], 
                                 compare_group_by=None, 
                                 filter_cond=None,
                                 compare_filter_cond=None,
-                                tol=0.0) %}
+                                tol=0.0) -%}
 
     {% set compare_model = model if not compare_model else compare_model %} 
     {% set compare_expression = expression if not compare_expression else compare_expression %} 
@@ -35,32 +34,27 @@
     {% set compare_group_by = group_by if not compare_group_by else compare_group_by %} 
 
     {% set n_cols = group_by|length %}
-
     with a as (
-
         {{ get_select(model, expression, filter_cond, group_by) }}
     ),
     b as (
-
         {{ get_select(compare_model, compare_expression, compare_filter_cond, compare_group_by) }}
-
     ),
     final as (
 
         select 
-            {% for i in range(1, n_cols + 1) %}
+            {%- for i in range(1, n_cols + 1) -%}
             coalesce(a.col_{{ i }}, b.col_{{ i }}) as col_{{ i }},
-            {% endfor %}
+            {%- endfor -%}
             abs(coalesce(a.expression, 0) - coalesce(b.expression, 0)) as exp_diff
         from
             a
             full outer join
             b on
-            {% for i in range(1, n_cols + 1) %}
-                a.col_{{ i }} = b.col_{{ i }} {% if not loop.last %} and {% endif %}
-            {% endfor %}
+            {%- for i in range(1, n_cols + 1) -%}
+                a.col_{{ i }} = b.col_{{ i }} {% if not loop.last %}and{% endif %}
+            {%- endfor -%}
     )
-
     select count(*) from final where exp_diff > {{ tol }}
 
-{% endmacro %}
+{%- endmacro -%}
