0768543cd659e73b2d4f86ac39b029d75778fab5
Claus Herther <claus@calogica.com>
2019-01-10 11:54:52 -0800

    Removes dependency on date dimensions and build date spin instead

--- macros/schema_tests/frequency.sql
+++ macros/schema_tests/frequency.sql
@@ -1,8 +1,17 @@
 {%- macro test_frequency(model, date_col, date_part="day", filter_cond=None) -%}
-with model_data as
+with day_dates as
+(
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="to_date('01/01/2015', 'mm/dd/yyyy')",
+        end_date="current_date"
+       )
+    }}
+),
+model_data as
 (
     select
-        {{ dbt_utils.date_trunc(date_part, date_col) }} as date_col,
+        {{ dbt_utils.date_trunc(date_part, date_col) }} as date_{{date_part}},
         count(*) as row_cnt
     from
         {{ model }} f
@@ -12,30 +21,34 @@ with model_data as
     group by
         1
 ),
-date_range as
+date_range as 
 (
-    select min(date_col) as start_date, max(date_col) as end_date from model_data
+    select 
+        min(date_{{date_part}}) as start_date, 
+        max(date_{{date_part}}) as end_date 
+    from model_data
 ),
-dates as
+date_part_dates as 
 (
     select
-        {{ dbt_utils.date_trunc(date_part, 'd.calendar_date') }} as date_col
+        {{ dbt_utils.date_trunc(date_part, 'date_day') }} as date_{{date_part}}
     from
-        {{ ref('d_date') }} d
+        day_dates d
         join
-        date_range r on d.calendar_date between r.start_date and r.end_date
-    group by 1
+        date_range dr on d.date_day between dr.start_date and dr.end_date
+    group by 
+        1
 ),
 final as
 (
     select
-        d.date_col,
-        case when f.date_col is null then true else false end as is_missing,
+        d.date_{{date_part}},
+        case when f.date_{{date_part}} is null then true else false end as is_missing,
         coalesce(f.row_cnt, 0) as row_cnt
     from
-        dates d
+        date_part_dates d
         left outer join
-        model_data f on d.date_col = f.date_col
+        model_data f on d.date_{{date_part}} = f.date_{{date_part}}
 )
 select count(*) from final where row_cnt = 0
 {%- endmacro -%}
