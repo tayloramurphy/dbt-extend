69a368ca0f92f945cee1a7e6d3449458c57c551c
Claus Herther <claus@calogica.com>
2019-01-29 20:02:35 -0800

    Brings back the test start/end date, thanks to Drew Banin!

--- macros/schema_tests/frequency.sql
+++ macros/schema_tests/frequency.sql
@@ -1,31 +1,36 @@
-{%- macro test_frequency(model, date_col, date_part="day", filter_cond=None) -%}
+{%- macro test_frequency(model, date_col, date_part="day", filter_cond=None, test_start_date=None, test_end_date=None) -%}
 {% if not execute %}
     {{ return('') }}
 {% endif %}
 
-{% call statement('date_range', fetch_result=True) %}
+{% if test_start_date is none and test_end_date is none %}
+    {% call statement('date_range', fetch_result=True) %}
 
-    select 
-        min({{ date_col }}) as start_date, 
-        max({{ date_col }}) as end_date 
-    from {{ model }} 
-    {% if filter_cond %}
-    where {{ filter_cond }}
-    {% endif %}
-    
-{% endcall %}
+        select 
+            min({{ date_col }}) as start_date, 
+            max({{ date_col }}) as end_date 
+        from {{ model }} 
+        {% if filter_cond %}
+        where {{ filter_cond }}
+        {% endif %}
 
-{%- set dr = load_result('date_range') -%}
-{%- set start_date = dr['data'][0][0] -%}
-{%- set end_date = dr['data'][0][1] -%}
-
-{% if start_date == None or end_date == None %}
-{{ log(msg='\nmodel ' ~ model ~ ' has no data for filter_cond=' ~ filter_cond ~ '\n', info=True) }}
-select 1
+    {% endcall %}
+    {%- set dr = load_result('date_range') -%}
 {% else %}
-{%- set start_date = start_date.strftime('%Y-%m-%d') -%}
-{%- set end_date = end_date.strftime('%Y-%m-%d') -%}
-    
+    {% call statement('date_expression', fetch_result=True) %}
+
+        select 
+            {{ test_start_date }} as start_date, 
+            {{ test_end_date }} as end_date 
+
+    {% endcall %}
+    {%- set dr = load_result('date_expression') -%}
+{% endif %}
+
+
+{%- set start_date = dr['data'][0][0].strftime('%Y-%m-%d') -%}
+{%- set end_date = dr['data'][0][1].strftime('%Y-%m-%d') -%}
+
 with day_dates as
 (
     {{ dbt_utils.date_spine(
@@ -69,5 +74,4 @@ final as
         model_data f on d.date_{{date_part}} = f.date_{{date_part}}
 )
 select count(*) from final where row_cnt = 0
-{% endif %}
 {%- endmacro -%}
