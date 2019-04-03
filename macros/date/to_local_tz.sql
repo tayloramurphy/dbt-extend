0a748b4be3db48f9184f7555ada05fe8e9342427
Claus Herther <claus@calogica.com>
2019-04-03 12:18:11 -0700

    Adds timestamp conversion to timezone

--- macros/date/to_local_tz.sql
+++ macros/date/to_local_tz.sql
@@ -1,4 +1,4 @@
 {%- macro to_local_tz(column, tz=None) -%}
 {% set tz = var("dbt_extend:time_zone") if not tz else tz %}
-convert_timezone('{{ tz }}', {{ column }})
+convert_timezone('{{ tz }}', {{ column }})::{{dbt_utils.type_timestamp()}}
 {%- endmacro -%}
